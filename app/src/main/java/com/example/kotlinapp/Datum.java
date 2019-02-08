b425b2b171832126a4ed300ed2408328d3aff30a
Mdzafar.Imam <mdzafar.imam@intelematics.com>
2019-02-08 15:55:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kotlinapp/Datum.java
@@ -0,0 +1,132 @@
+
+package com.example.kotlinapp;
+
+import javax.annotation.Generated;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("org.jsonschema2pojo")
+public class Datum implements Parcelable
+{
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("first_name")
+    @Expose
+    private String firstName;
+    @SerializedName("last_name")
+    @Expose
+    private String lastName;
+    @SerializedName("avatar")
+    @Expose
+    private String avatar;
+    public final static Parcelable.Creator<Datum> CREATOR = new Creator<Datum>() {
+
+
+        @SuppressWarnings({
+            "unchecked"
+        })
+        public Datum createFromParcel(Parcel in) {
+            Datum instance = new Datum();
+            instance.id = ((int) in.readValue((int.class.getClassLoader())));
+            instance.firstName = ((String) in.readValue((String.class.getClassLoader())));
+            instance.lastName = ((String) in.readValue((String.class.getClassLoader())));
+            instance.avatar = ((String) in.readValue((String.class.getClassLoader())));
+            return instance;
+        }
+
+        public Datum[] newArray(int size) {
+            return (new Datum[size]);
+        }
+
+    }
+    ;
+
+    /**
+     * 
+     * @return
+     *     The id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * @param id
+     *     The id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * 
+     * @return
+     *     The firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * 
+     * @param firstName
+     *     The first_name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * 
+     * @return
+     *     The lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * 
+     * @param lastName
+     *     The last_name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * 
+     * @return
+     *     The avatar
+     */
+    public String getAvatar() {
+        return avatar;
+    }
+
+    /**
+     * 
+     * @param avatar
+     *     The avatar
+     */
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(id);
+        dest.writeValue(firstName);
+        dest.writeValue(lastName);
+        dest.writeValue(avatar);
+    }
+
+    public int describeContents() {
+        return  0;
+    }
+
+}
---
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.kotlinapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initUI()
+
+
+
+    }
+
+    private fun initUI() {
+        val editEmail = findViewById<EditText>(R.id.editEmail)
+        val editPassword = findViewById<EditText>(R.id.editPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val textFogotPswd = findViewById<TextView>(R.id.textFogotPswd)
+        val textNewUser = findViewById<TextView>(R.id.textNewUser)
+
+        val email = editEmail.text
+        val pswd = editPassword.text
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this,SecondActivity::class.java))
+        }
+    }
+}
