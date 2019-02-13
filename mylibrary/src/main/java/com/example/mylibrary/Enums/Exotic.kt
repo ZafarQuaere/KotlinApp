c31b22d81a58b353bb890dabfe1781c5a48e56e2
Mdzafar.Imam <mdzafar.imam@intelematics.com>
2019-02-13 17:53:04 +0530

    some more classes added.

---
+++ mylibrary/src/main/java/com/example/mylibrary/Enums/Exotic.kt
@@ -0,0 +1,5 @@
+package com.example.mylibrary.Enums
+
+interface Exotic {
+    fun isExotic() :Boolean
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/functionalProgramming/typealias.kt
@@ -0,0 +1,24 @@
+package com.example.mylibrary.functionalProgramming
+
+interface Machine<T> {
+    fun process(product: T)
+}
+
+fun <T> useMachine(t: T, machine: Machine<T>) {
+    machine.process(t)
+}
+
+class PrintMachine<T> : Machine<T> {
+    override fun process(t: T) {
+        println(t)
+    }
+}
+
+fun main(args: Array<String>) {
+    useMachine(5, PrintMachine())
+    useMachine(5, object : Machine<Int> {
+        override fun process(t: Int) {
+            println(t)
+        }
+    })
+}
