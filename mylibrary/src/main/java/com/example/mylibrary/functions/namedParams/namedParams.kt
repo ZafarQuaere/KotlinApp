cc5ffd4b8253a92083f2ec9b13793b43f51999a8
Zafar Imam <zafarimam512@gmail.com>
2019-02-16 23:55:11 +0530

    conditional and looping statements.

---
+++ mylibrary/src/main/java/com/example/mylibrary/functions/namedParams/namedParams.kt
@@ -0,0 +1,7 @@
+package com.example.mylibrary.functions.namedParams
+
+import com.example.mylibrary.functions.defaultFunction.findVolume
+
+fun main(args: Array<String>) {
+    findVolume(height = 10, length = 90,breadth = 25)
+}
