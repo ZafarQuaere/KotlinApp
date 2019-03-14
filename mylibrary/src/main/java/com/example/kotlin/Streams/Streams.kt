9e2102dc0f51149fb1fe22a8ea202f5587a3ff61
Mdzafar.Imam <mdzafar.imam@intelematics.com>
2019-03-14 15:11:26 +0530

    Some more classes added.

---
+++ mylibrary/src/main/java/com/example/kotlin/Streams/Streams.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin.Streams
+
+import java.util.stream.Collectors
+
+
+fun main(args: Array<String>) {
+  /*  val stream = 1.rangeTo(10).asSequence().asStream()
+    val resultantList = stream.skip(5).collect(Collectors.toList<Int>())
+    println(resultantList)*/
+}
